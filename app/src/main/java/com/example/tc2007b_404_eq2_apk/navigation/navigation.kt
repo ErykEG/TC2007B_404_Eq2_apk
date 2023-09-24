d06d1163e064719d9fa310fa4a024a073b2f7817
Oscar <oscaritoariel_23of@hotmail.com>
2023-09-24 17:33:15 -0600

    HomePage Commit

--- app/src/main/java/com/example/tc2007b_404_eq2_apk/navigation/navigation.kt
+++ app/src/main/java/com/example/tc2007b_404_eq2_apk/navigation/navigation.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
@@ -19,7 +18,6 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.Create
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Info
--- app/src/main/java/com/example/tc2007b_404_eq2_apk/screens/home/HomePage.kt
+++ app/src/main/java/com/example/tc2007b_404_eq2_apk/screens/home/HomePage.kt
@@ -1,20 +1,85 @@
 package com.example.tc2007b_404_eq2_apk.screens.home
 
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import com.example.tc2007b_404_eq2_apk.viewModel.AppViewModel
 
+
 @Composable
-fun HomePage(viewModel: AppViewModel) {
+fun HomePage(appviewModel: AppViewModel) {
+    val oscList: List<String> = listOf(
+        "Arena",
+        "Mayama AC",
+        "Fundación Tuk"
+    )
 
+    Surface(
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(modifier = Modifier.padding(12.dp)) {
+            LazyColumn {
+                items(items = oscList) {
+                    OSCRow(osc = it) { osc ->
+                        Log.d("OSC", "$osc")
+                    }
+                }
+            }
+        }
+    }
+}
 
-    /*val loggedIn = remember {
-        mutableStateOf(viewModel.isLoggedIn.value)
-    }*/
+@Composable
+fun OSCRow(osc: String, onItemClick: (String) -> Unit = {}) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp)
+            .clickable {
+                onItemClick(osc)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+    ) {
 
-    Column {
-        Text(text = "Welcome to HomePage")
-        //Text(text = "Is Logged In: ${viewModel.isUserLoggedIn()}")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape
+            ) {
+
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = null)
+            }
+            Text(text = osc)
+        }
     }
 }
