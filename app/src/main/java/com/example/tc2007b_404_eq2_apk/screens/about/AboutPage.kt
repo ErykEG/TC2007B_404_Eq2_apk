a2a396c3640c2a60a3828d458cae41535fc8b122
A00832425 <113365949+A00832425@users.noreply.github.com>
2023-09-27 15:30:06 -0600

    Update AboutPage.kt

--- app/src/main/java/com/example/tc2007b_404_eq2_apk/screens/about/AboutPage.kt
+++ app/src/main/java/com/example/tc2007b_404_eq2_apk/screens/about/AboutPage.kt
@@ -1,14 +1,21 @@
 package com.example.tc2007b_404_eq2_apk.screens.about
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -20,68 +27,81 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tc2007b_404_eq2_apk.viewModel.AppViewModel
+import kotlinx.coroutines.launch
 
 
-
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AboutPage(appviewModel: AppViewModel) {
 
 
 
+    val corutineScope = rememberCoroutineScope()
 
     var cardNumber by remember { mutableStateOf("") }
     var cardExpiration by remember { mutableStateOf("") }
     var cardCVC by remember { mutableStateOf("") }
-
-
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-
-        Text("Donate", fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
-
-        TextField(value = cardNumber, onValueChange = {
-            cardNumber = it
-        }, placeholder = {
-            Text("Card Number")
-        })
-
-        TextField(
-            value = cardExpiration,
-            onValueChange = {
-                cardExpiration = it
-            },
-            placeholder = {
-                Text("Expiration")
-            },
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password)
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(
+            hostState = snackbarHostState,
+            modifier = Modifier.padding(16.dp)
         )
-
-        TextField(value = cardCVC, onValueChange = {
-            cardCVC = it
-        }, placeholder = {
-            Text("cardCVC")
-        })
-
-        Button(onClick = {
-
-
-
-
-        }) {
-            Text(text = "Pagar")
         }
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+
+            Text("Donate", fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
+
+            TextField(value = cardNumber, onValueChange = {
+                cardNumber = it
+            }, placeholder = {
+                Text("Card Number")
+            })
+
+            TextField(
+                value = cardExpiration,
+                onValueChange = {
+                    cardExpiration = it
+                },
+                placeholder = {
+                    Text("Expiration")
+                },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password)
+            )
+
+            TextField(value = cardCVC, onValueChange = {
+                cardCVC = it
+            }, placeholder = {
+                Text("cardCVC")
+            })
+
+            Button(onClick = {
+                corutineScope.launch {
+                    snackbarHostState.showSnackbar("Pago Realizado ")
+                }
 
 
-        // Text("${loginResult.token}  ")
-        //Text(" ${loginResult.message}")
+            }) {
+                Text(text = "Pagar")
+            }
 
 
+            // Text("${loginResult.token}  ")
+            //Text(" ${loginResult.message}")
+
+
+        }
     }
+
+
+
 }
 
 
