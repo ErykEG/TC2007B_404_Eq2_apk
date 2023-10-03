c876686b6092f5d9f4dcc4cc6bc751a29318a0ee
Oscar <oscaritoariel_23of@hotmail.com>
2023-10-02 22:15:19 -0600

    Eliminación de imports

--- app/src/main/java/com/example/tc2007b_404_eq2_apk/MainActivity.kt
+++ app/src/main/java/com/example/tc2007b_404_eq2_apk/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
