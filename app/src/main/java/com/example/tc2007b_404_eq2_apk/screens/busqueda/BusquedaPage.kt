a70d154685bbf1d9929e797647cafaf4a97ec1fa
Oscar <oscaritoariel_23of@hotmail.com>
2023-10-09 18:57:35 -0600

    Eliminar import

--- app/src/main/java/com/example/tc2007b_404_eq2_apk/screens/busqueda/BusquedaPage.kt
+++ app/src/main/java/com/example/tc2007b_404_eq2_apk/screens/busqueda/BusquedaPage.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
