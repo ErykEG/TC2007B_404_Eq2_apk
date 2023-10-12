be86e0358a50fc6ac4608752568209d9553e5cc4
Oscar <oscaritoariel_23of@hotmail.com>
2023-10-11 19:48:16 -0600

    CargaFavoritos

--- app/src/main/java/com/example/tc2007b_404_eq2_apk/screens/favoritos/FavoritosPage.kt
+++ app/src/main/java/com/example/tc2007b_404_eq2_apk/screens/favoritos/FavoritosPage.kt
@@ -1,10 +1,14 @@
 package com.example.tc2007b_404_eq2_apk.screens.favoritos
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -18,6 +22,7 @@ import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,18 +36,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.tc2007b_404_eq2_apk.R
 import com.example.tc2007b_404_eq2_apk.model.OrgResp
 import com.example.tc2007b_404_eq2_apk.model.OrgRespList
 import com.example.tc2007b_404_eq2_apk.navigation.Screens
+import com.example.tc2007b_404_eq2_apk.screens.home.OSCRow
 import com.example.tc2007b_404_eq2_apk.service.UserService
 import com.example.tc2007b_404_eq2_apk.viewModel.AppViewModel
 import com.example.tc2007b_404_eq2_apk.viewModel.UserViewModel
+import com.skydoves.landscapist.glide.GlideImage
 
 @SuppressLint("MutableCollectionMutableState")
 @Composable
 fun FavoritosPage(navController: NavController, appViewModel: AppViewModel) {
     val favorites = appViewModel.favorites.filter { it.value }
     val userViewModel = UserViewModel(UserService.instance)
+    var visibility by remember { mutableStateOf(true) }
     var favList by remember {
         mutableStateOf(OrgRespList())
     }
@@ -56,12 +65,37 @@ fun FavoritosPage(navController: NavController, appViewModel: AppViewModel) {
             }
         }
     }
+
     Column(modifier = Modifier.padding(12.dp)) {
         LazyColumn {
             items(items = favList) {
+                visibility = false
                 OSCRow(osc = it) { osc ->
                     navController.navigate(route = Screens.DetailsOSC.name + "/$osc")
                 }
+            }
+        }
+    }
+
+    AnimatedVisibility(
+        visible = visibility,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                GlideImage(
+                    imageModel = R.drawable.logoloading,
+                    modifier = Modifier.size(200.dp),
+                    contentDescription = null
+                )
             }
         }
     }
